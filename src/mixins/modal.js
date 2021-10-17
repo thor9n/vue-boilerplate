df053cb46c281e406578686aeb237118dd502559
Niklas <niklas.thorin@crisby.se>
2021-10-17 17:37:55 +0200

    added modal + misc css fixes

---
+++ src/mixins/modal.js
@@ -0,0 +1,19 @@
+import { $vfm } from 'vue-final-modal'
+// import ChatBotModal from '@/components/chat-bot/chat-bot-modal.vue'
+// import PrivacyPolicy from '@/router/pages/Privacy.vue'
+
+export default {
+    methods: {
+        // EbbaModal() {
+        //     $vfm.show('modal', {
+        //         component: ChatBotModal,
+        //     })
+        // },
+        // PrivacyModal() {
+        //     $vfm.show('modal', {
+        //         component: PrivacyPolicy,
+        //         width: '1200'
+        //     })
+        // },
+    }
+}
